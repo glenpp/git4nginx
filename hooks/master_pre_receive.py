09f70fe6d953df21b96725fe155ba404b1337379
Glen Pit-Pladdy <glenpp@users.noreply.github.com>
2019-08-03 17:15:00 +0100

    plugin based hook

--- hooks/master_pre_receive.py
+++ hooks/master_pre_receive.py
@@ -23,26 +23,11 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import os
 import sys
-import yaml
-import subprocess
-
-
-class GitWrapper(object):
-    zero = '0000000000000000000000000000000000000000'
-
-    def __init__(self, git_dir):
-        self.git_dir = os.path.realpath(git_dir)
-        self.git_command = ['git', '--git-dir={}'.format(self.git_dir)]
-
-    def get_revisions(self, revision_range):
-        log_output = subprocess.check_output(self.git_command + ['log', '--pretty=oneline', revision_range])
-        revisions = [log.split(b' ')[0].decode('ascii') for log in log_output.splitlines()]
-        return revisions
-        # for checking rev in branch:
-        #   44f15621ecf9ee860ec7ac3ea422c002c296f35f^..master
-        #       no output with rev not in branch
-        #   44f15621ecf9ee860ec7ac3ea422c002c296f35f^..dev
-        #       output from this ref with rev in branch
+import logging
+import imp
+# custom helper needs us to find the path first
+sys.path.append(os.path.realpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..')))
+import hook_helper
 
 
 def log_abort(message, exit_status=1):
@@ -50,47 +35,54 @@ def log_abort(message, exit_status=1):
     if exit_status is not None:
         sys.exit(exit_status)
 
-def load_config():
-    """Load the config file specified in uwsgi environment GITWRAPPER_CONFIG
 
-    :return: config contents (should be dict)
-    """
-    if '_GITWRAPPER_CONFIG' not in os.environ:
-        raise KeyError("Require environment variable to be set: _GITWRAPPER_CONFIG")
-    config_path = os.environ['_GITWRAPPER_CONFIG']
-    # read config
-    try:
-        with open(config_path, 'rt') as f_conf:
-            config = yaml.safe_load(f_conf)
-    except FileNotFoundError:
-        log_abort("Configuration file not found: {}".format(config_path))
-    except yaml.parser.ParserError as exc:
-        log_abort("Configuration file yaml error:\n{}".format(exc))
-    # sanity check config
-    if 'authentication' not in config or not isinstance(config['authentication'], dict):
-        log_abort("Configuration file does not contain 'authentication' map")
-    if 'authorisation' not in config or not isinstance(config['authorisation'], dict):
-        log_abort("Configuration file does not contain 'authorisation' map")
-    # TODO more checks
-    return config
 
 
 def main(argv):
-    config = load_config()
-    # work out main hook dir
-    repo = os.getcwd()
-    hook_name = os.path.basename(argv[0])
-    master_hook = os.path.realpath(argv[0])
-    hook_dir = os.path.dirname(master_hook)
-    plugins_dir = os.path.join(hook_dir, 'plugins')
-    # setup for plugins
-    git = GitWrapper(repo)
-    # TODO probably want to resolve user and groups from REMOTE_USER
-    # TODO work out what plugins apply to this repo
+    hook_helper.setup_loggger()
+    config = hook_helper.load_config()
+    hook_name = argv[0].split('/')[-1]
+    master_hook_dir = os.path.dirname(os.path.realpath(__file__))
+    plugin_dir = hook_name.replace('-', '_') + '_plugins'
+    plugin_dir = os.path.join(master_hook_dir, plugin_dir)
+    # get the references list from stdin
+    references = [line.strip().split(' ') for line in sys.stdin]
+    # prepare plugin info
+    if 'REMOTE_USER' not in os.environ:
+        hook_helper.log_abort("No REMOTE_USER in environment")
+    username = os.environ['REMOTE_USER']
+    if username not in config['authentication']:
+        hook_helper.log_abort("User %s not in authentication config", username)
+    user_authentication = config['authentication'][username]
+    groups = user_authentication['groups'] if 'groups' in user_authentication else []
+    inputs = [argv, references]
+    gitwrapper = hook_helper.GitWrapper()
+    # load & run plugins
+    for plugin_file in sorted(os.listdir(plugin_dir)):
+        logging.debug(plugin_file)
+        plugin_name, ext = os.path.splitext(plugin_file)
+        if ext != '.py':
+            continue
+        if '.' + plugin_name not in config['hooks']:  # TODO needs to search for this repo, check enabled
+            # plugin name not enabled
+            continue
+        plugin_config = config['hooks']['.' + plugin_name]    # TODO needs to search for this repo
+        logging.info("Loading plugin: %s", plugin_name)
+        info = imp.find_module(plugin_name, [plugin_dir])
+        plugin = imp.load_module(plugin_name, *info)
+        logging.info("Executing plugin: %s", plugin_name)
+        plugin_obj = plugin.Plugin(
+            username,
+            groups,
+            user_authentication,
+            plugin_config,
+            config,
+            inputs,
+            gitwrapper
+        )
+        plugin_obj.run()
 
 
-    # TODO proxy to user_hooks/
-    sys.exit(0)
 
 
 if __name__ == '__main__':
