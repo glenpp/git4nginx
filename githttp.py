b515807d78867ec24ee3edf8391ce6be7904c7a7
Glen Pit-Pladdy <glenpp@users.noreply.github.com>
2019-08-09 22:38:26 +0100

    authentication via plugins

--- githttp.py
+++ githttp.py
@@ -20,11 +20,14 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import logging
-import yaml
 import re
 import tempfile
 import subprocess
+import imp
 import os
+import json
+import base64
+import yaml
 
 
 
@@ -32,16 +35,27 @@ import flask
 app = flask.Flask(__name__)
 ## for this app config must be loaded here before it's used for module paths below
 #app.config.from_object('config')    # loads config.py
+# TODO do better
 # ensure our logger is used see http://y.tsutsumi.io/global-logging-with-flask.html
 logging.basicConfig(
     format='[%(asctime)s] %(levelname)s %(filename)s:%(lineno)d - %(message)s',
     datefmt='%c',
-#    level=logging.INFO,
+    #level=logging.INFO,
     level=logging.DEBUG,
 )
 app.logger.handlers = []
 app.logger.propagate = True
 
+
+@app.errorhandler(401)
+def authentication_401(error):
+    """Custom authentication response
+    """
+    return flask.Response(
+        error.description,
+        error.code,
+        {'WWW-Authenticate': 'Basic realm="Authentication Required", charset="UTF-8"'}
+    )
 
 
 
@@ -53,6 +67,12 @@ app.logger.propagate = True
 @app.route('/<string:project_group>/<string:project>.git')
 @app.route('/<string:project_group>/<string:project>.git/<path:sub_path>', methods=['GET', 'POST'])
 def githandler(project, project_group=None, sub_path=None):
+    """Main git http entry point
+
+    :arg project: str, project name with .git removed
+    :arg project_group: str|None, project group (directory) or None for top level
+    :arg sub_path: str|None, any path elements below the repo requested
+    """
     # sanity check inputs - limit these within known safe bounds
     if len(project) > 64:
         app.logger.error("Error: Project names are limited to 64 characters (before .git)")
@@ -83,48 +103,95 @@ def githandler(project, project_group=None, sub_path=None):
     else:
         app.logger.critical("Unexpected HTTP METHOD: %s", flask.request.method)
         flask.abort(400)
+    flask.abort(401)
 
     # load config
     config = load_config()
     # work out the type of operation
     is_write = False if flask.request.args.get('service') != 'git-receive-pack' and sub_path != 'git-receive-pack' else True
 
-
-    # TODO custom auth
-    authenticated_user = flask.request.remote_user
+    # work out available authentication
+    username = flask.request.remote_user
+    password = None
+    if username is None:
+        # then we didn't get authentication from upstream so we do it ourselves
+        app.logger.info("Authentication no provided from upstream so performing headers...")
+        auth_header = flask.request.headers.get('authorization')
+        if auth_header is None:
+            app.logger.info("No authentication headers for Basic. Prompting client.")
+            flask.abort(401)
+        elif not auth_header.startswith('Basic '):
+            app.logger.error("Authentication header does not start \"Basic\"")
+            flask.abort(400)
+        else:
+            auth_info = base64.b64decode(auth_header[6:])
+            username, password = auth_info.decode('utf-8').split(':', 1)
+    # if we failed to get any user then we most likley have a configuration problem
+    if username is None:
+        app.logger.critical("No username can be determined for authentication. Configuration problem?")
+        flask.abort(500)
+    # authentication plugin
+    auth_plugin_name = config['authentication']['plugin']
+    app.logger.info("Loading authentication plugin: %s", auth_plugin_name)
+    auth_plugin_path = os.path.dirname(os.path.realpath(__file__))
+    auth_plugin_path = os.path.join(auth_plugin_path, 'authentication_plugins')
+    info = imp.find_module(auth_plugin_name, [auth_plugin_path])
+    auth_plugin = imp.load_module(auth_plugin_name, *info)
+    app.logger.info("Checking authentication with plugin...")
+    authenticated, groups, info = auth_plugin.authenticate(app.logger, config['authentication']['plugin_config'], username, password)
+    if authenticated:
+        authenticated_user = username
+        app.logger.info("Successful authentication for user: %s", authenticated_user)
+    else:
+        app.logger.warning("Authentication failed for user: %s", username)
+        flask.abort(401)
 
 
     # logging details for now
-    app.logger.info("handler: {} {} / {} / {}".format(flask.request.method, project_group, project, sub_path))
-    app.logger.info("        :: {}".format(authenticated_user))
-    if flask.request.args:
-        app.logger.info("        :: {}".format(flask.request.args))
-    app.logger.info("        : write {}".format(is_write))
+    app.logger.info("handler: %s %s / %s / %s", flask.request.method, project_group, project, sub_path)
+    app.logger.info("        :: %s", authenticated_user)
+    app.logger.info("        :: %s", groups)
+    app.logger.info("        : write %s", is_write)
 
     # determine and log permission for this request
-    permission = Permissions(config, authenticated_user).get_permission(project_group, project)
+    permission = Permissions(config['authorisation'], authenticated_user, groups).get_permission(project_group, project)
     if permission['write'] and permission['read']:
-        app.logger.info("User {} has permissions for read & write on: {}/{}".format(authenticated_user, project_group, project))
+        app.logger.info("User %s has permissions for read & write on: %s/%s", authenticated_user, project_group, project)
     elif permission['write']:
-        app.logger.info("User {} has permissions for write on: {}/{}".format(authenticated_user, project_group, project))
+        app.logger.info("User %s has permissions for write on: %s/%s", authenticated_user, project_group, project)
     elif permission['read']:
-        app.logger.info("User {} has permissions for read on: {}/{}".format(authenticated_user, project_group, project))
+        app.logger.info("User %s has permissions for read on: %s/%s", authenticated_user, project_group, project)
     if is_write and not permission['write']:
-        app.logger.warning("User {} does not have write permission for: {}/{}".format(authenticated_user, project_group, project))
+        app.logger.warning("User %s does not have write permission for: %s/%s", authenticated_user, project_group, project)
         flask.abort(403)
     elif not permission['read']:
-        app.logger.warning("User {} does not have read permission for: {}/{}".format(authenticated_user, project_group, project))
+        app.logger.warning("User %s does not have read permission for: %s/%s", authenticated_user, project_group, project)
         flask.abort(403)
 
-    # run the cgi-bin with wrapper
+    # sanity check repo exists
+    repo_path = config['repo_root']
+    if not os.path.isdir(repo_path):
+        app.logger.critical("Configured repo_root does not exist: %s", repo_path)
+        flask.abort(500)
+    if project_group is not None:
+        repo_path = os.path.join(repo_path, project_group)
+    repo_path = os.path.join(repo_path, project)
+    if not os.path.isdir(repo_path):
+        app.logger.warning("Requested repo does not exist: %s", repo_path)
+        flask.abort(404)
+
+    # run the cgi-bin with wrapper with appropriate environment variables
     extra_env = {
         'GIT_PROJECT_ROOT': config['repo_root'],
         'GIT_HTTP_EXPORT_ALL': '',
+        # user details exposed to hooks
+        'REMOTE_USER': authenticated_user,  # may already be in uwsgi env
+        'GIT4NGINX_GROUPS': json.dumps(groups),
+        'GIT4NGINX_INFO': json.dumps(info),
     }
-    # TODO if we identify the user set REMOTE_USER
     if flask.request.method == 'POST':
         with HookLogDir() as temp_log_dir:
-            extra_env['GITGLUE_LOG_DIR'] = temp_log_dir
+            extra_env['GIT4NGINX_LOG_DIR'] = temp_log_dir
             response = cgi_wrapper(config['bin_path'], extra_env, flask.request.data)   # TODO switch to flask.request.stream for input
         return response
     return cgi_wrapper(config['bin_path'], extra_env)
@@ -136,22 +203,13 @@ def githandler(project, project_group=None, sub_path=None):
 class Permissions(object):
     """Calculate permissions
     """
-    def __init__(self, config, username):
+    def __init__(self, authorisation_config, username, groups):
         """Calculate permissions
         """
-        self.config = config
+        self.authorisation_config = authorisation_config
         self.username = username
         self.user_authentication = None
-        self.groups = None
-        # resolve authentication
-        if self.username not in config['authentication']:
-            app.logger.warning("Username not found: {}".format(self.username))
-            return
-        self.user_authentication = config['authentication'][self.username]
-        if 'groups' in self.user_authentication:
-            self.groups = set(self.user_authentication['groups'])
-        else:
-            self.groups = set()
+        self.groups = set(groups)
 
     def _check_node(self, node, permission):
         """Calculate permissions for a node
@@ -188,41 +246,41 @@ class Permissions(object):
             'read': False,
             'write': False,
         }
-        authorisation = self.config['authorisation']
+        authorisation = self.authorisation_config
         # check project group
         if project_group not in authorisation:
-            app.logger.info("Project Group no authorisation: {}".format(project_group))
+            app.logger.info("Project Group no authorisation: %s", project_group)
             return permission
         authorisation = authorisation[project_group]
         if self._check_node(authorisation, permission):
             return permission
         # check project
         if project not in authorisation:
-            app.logger.info("Project no authorisation: {}".format(project))
+            app.logger.info("Project no authorisation: %s", project)
             return permission
         authorisation = authorisation[project]
         if self._check_node(authorisation, permission):
             return permission
         return permission
-        
+
 
 
 def load_config():
-    """Load the config file specified in uwsgi environment GITGLUE_CONFIG
+    """Load the config file specified in uwsgi environment GIT4NGINX_CONFIG
 
     :return: config contents (should be dict)
     """
-    if 'GITGLUE_CONFIG' not in flask.request.environ:
-        app.logger.critical("Configuration file not configured: GITGLUE_CONFIG must be in uwsgi environment")
+    if 'GIT4NGINX_CONFIG' not in flask.request.environ:
+        app.logger.critical("Configuration file not configured: GIT4NGINX_CONFIG must be in uwsgi environment")
         flask.abort(500)
     try:
-        with open(flask.request.environ['GITGLUE_CONFIG'], 'rt') as f_conf:
+        with open(flask.request.environ['GIT4NGINX_CONFIG'], 'rt') as f_conf:
             config = yaml.safe_load(f_conf)
     except FileNotFoundError:
-        app.logger.critical("Configuration file not found: {}".format(flask.request.environ['GITGLUE_CONFIG']))
+        app.logger.critical("Configuration file not found: %s", flask.request.environ['GIT4NGINX_CONFIG'])
         flask.abort(500)
     except yaml.parser.ParserError as exc:
-        app.logger.critical("Configuration file yaml error:\n{}".format(exc))
+        app.logger.critical("Configuration file yaml error:\n%s", exc)
         flask.abort(500)
     # sanity check
     if 'authentication' not in config or not isinstance(config['authentication'], dict):
@@ -236,6 +294,8 @@ def load_config():
 
 
 class HookLogDir(object):
+    """Context for log directory for hooks to pass back logging
+    """
     level2int = {
         'DEBUG': logging.DEBUG,
         'INFO': logging.INFO,
@@ -244,27 +304,33 @@ class HookLogDir(object):
         'CRITICAL': logging.CRITICAL,
     }
     def __init__(self):
+        """Setup
+        """
         self._path = None
     def __enter__(self):
+        """Enter context - create temporary directory (and store)
+        """
         self._path = tempfile.mkdtemp()
         return self._path
     def __exit__(self, exc_type, exc_value, traceback):
+        """Exit context - process logs in temporary directory, clean up
+        """
         # re-log hook log lines
         for log in sorted(os.listdir(self._path)):
             app.logger.debug("start logfile: %s", log)
-            epoch, hook = log.split('_', 1)
+            _, hook = log.split('_', 1)
             log_path = os.path.join(self._path, log)
             with open(log_path, 'rt') as f_log:
                 for line in f_log:
                     match = re.match(r'^(\d+\.\d+)\s+\[(\w+)\]\s+(\S.*)\s+\(([^\(\)]+)\)$', line.strip())
                     if not match:
-                        app.logger.critical("Can't parse hook log %s line: %s", log, line)
+                        app.logger.critical("%s - Can't parse hook log %s line: %s", hook, log, line)
                         continue
                     if match.group(2) not in self.level2int:
-                        app.logger.critical("Don't have a log level match for %s in line: %s", match.group(2), line)
+                        app.logger.critical("%s - Don't have a log level match for %s in line: %s", hook, match.group(2), line)
                         continue
                     level = self.level2int[match.group(2)]
-                    app.logger.log(level, match.group(3))
+                    app.logger.log(level, "%s - %s", hook, match.group(3))
             app.logger.debug("end logfile: %s", log)
             os.unlink(log_path)
         os.rmdir(self._path)
@@ -284,7 +350,7 @@ def cgi_wrapper(bin_path, extra_env=None, data=None):
         for key, value in flask.request.environ.items()
         if not key.startswith(('uwsgi.', 'werkzeug.', 'wsgi.'))
     }
-    cgienv['SERVER_SOFTWARE'] = 'cgiwrapper'
+    cgienv['SERVER_SOFTWARE'] = 'git4nginx'
     cgienv['GATEWAY_INTERFACE'] = 'CGI/1.1'
     if extra_env is not None:
         cgienv.update(extra_env)
@@ -308,12 +374,12 @@ def cgi_wrapper(bin_path, extra_env=None, data=None):
         else:
             stdout, stderr = proc.communicate(data)
     if proc.returncode != 0:
-        app.logger.debug("{} returned {:d}".format(bin_path, proc.returncode))
-        app.logger.debug("{} stdout:\n{}\n--- end stderr".format(bin_path, stdout))
-        app.logger.debug("{} stderr:\n{}\n--- end stderr".format(bin_path, stderr))
+        app.logger.debug("%s returned %d", bin_path, proc.returncode)
+        app.logger.debug("%s stdout:\n%s\n--- end stderr", bin_path, stdout)
+        app.logger.debug("%s stderr:\n%s\n--- end stderr", bin_path, stderr)
         flask.abort(500)
     elif stderr:
-        app.logger.debug("{} stderr:\n{}\n--- end stderr".format(bin_path, stderr))
+        app.logger.debug("%s stderr:\n%s\n--- end stderr", bin_path, stderr)
     # split and handle header from cgi
     header, body = tuple(stdout.split(b'\r\n\r\n', 1))
     status_code = 200
